3b244fbd9a63aa374063cfa748b663bc03d9d224
jayvardhish <jayvardhishsharma46@gmail.com>
2025-08-05 09:18:58 +0530

    Add enhanced SastaSmart frontend component and update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import EnhancedSastaSmart from './components/SastaSmartEnhanced';
 
 function App() {
-  const [deals, setDeals] = useState([]);
-
-  useEffect(() => {
-    fetch("https://sastasmart-backend.onrender.com/api/deals")
-      .then((res) => res.json())
-      .then((data) => setDeals(data));
-  }, []);
-
   return (
     <div>
-      <h1>SastaSmart Deals</h1>
-      <ul>
-        {deals.map((deal, index) => (
-          <li key={index}>{deal.title}</li>
-        ))}
-      </ul>
+      <EnhancedSastaSmart />
     </div>
   );
 }
